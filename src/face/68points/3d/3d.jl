01bade651f60708315b4e2cb4df37e569934afb8
quildtide <42811940+quildtide@users.noreply.github.com>
2021-03-17 23:36:42 -0400

    Fix additional typo

--- src/face/68points/3d/3d.jl
+++ src/face/68points/3d/3d.jl
@@ -4,6 +4,6 @@ abstract type Abstract3d68PointVentr <: Abstract68PointVentr end
 
 dim_n(::Abstract3d68PointVentr) = 3
 
-_raw_openness(::Abstract68Point3dVentr, points::AbstractArray, ind1, ind2) = sum(_raw_dist.((points,), ind1, ind2))
+_raw_openness(::Abstract3d68PointVentr, points::AbstractArray, ind1, ind2) = sum(_raw_dist.((points,), ind1, ind2))
 
  
